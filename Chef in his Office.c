fe26b4cc815f67d38cec639aa030b55f886a5c2d
DISHVANTHINI <150140881+dishvanthini@users.noreply.github.com>
2024-12-16 14:29:50 +0530

    Create Chef in his Office.c

---
+++ Chef in his Office.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int t;
+    scanf("%d",&t);
+    while(t--){
+        int x,y,tot;
+        scanf("%d %d",&x,&y);
+        tot=(x*4)+y;
+        printf("%d\n",tot);
+        
+    }
+	
+return 0;
+}
+
